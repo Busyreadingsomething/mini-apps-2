819ff8425dbf0487ad75cf9f3b40fc9ca4ef66f9
Makan Azarshahy <oro1919@gmail.com>
2018-04-04 19:58:11 -0700

    Added render of application to file

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ChartView from './components/ChartView.jsx';
 
-
+ReactDOM.render(<ChartView />, document.getElementById('app'));
