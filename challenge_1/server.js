502c5485725916aca53f97fe46f2a2b0a64477ff
Makan Azarshahy <oro1919@gmail.com>
2018-04-04 19:59:19 -0700

    Added routes, static serving, and server to file

--- challenge_1/server.js
+++ challenge_1/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const coinRoute = require('./routes/coinRoute');
 
+const app = express();
 
+mongoose.connect('mongodb://localhost/coinDB');
+
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+
+app.use('/crypto', coinRoute);
+
+app.listen(3000, () => console.log('Example app'));
