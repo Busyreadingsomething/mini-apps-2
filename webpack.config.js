27c47570c031dc2846127773e01065e8999025f6
Makan Azarshahy <oro1919@gmail.com>
2018-04-06 14:06:03 -0700

    Added mini-3 bundle in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,16 @@ const mini2 = {
   },
 };
 
+const mini3 = {
+  entry: path.resolve(__dirname, './challenge_3/client/app.jsx'),
+  output: {
+    filename: 'mini-3-bundle.js',
+    path: path.resolve(__dirname, './challenge_3/public/dist'),
+  },
+};
 
 module.exports = [
   Object.assign({}, common, mini1),
   Object.assign({}, common, mini2),
+  Object.assign({}, common, mini3),
 ];
