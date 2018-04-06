652d42ca358c05ccfdd9932eb85c258c9028e768
Makan Azarshahy <oro1919@gmail.com>
2018-04-05 19:48:46 -0700

    Added basic app

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CheckoutView from './components/CheckoutView';
 
-
+ReactDOM.render(<CheckoutView />, document.getElementById('app'));
