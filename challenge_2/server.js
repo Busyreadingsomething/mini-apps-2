9db2d8027a72c6c734c0d9978c2694a0c4a236ea
Makan Azarshahy <oro1919@gmail.com>
2018-04-05 19:49:39 -0700

    Added basic server with routes. Connectd to mongo

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const dbRoute = require('./routes/dbRoute');
 
+mongoose.connect('mongodb://localhost/sales');
 
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+
+app.use('/checkout', dbRoute);
+
+app.listen(3001, () => console.log('Listening to Port 3001'));
