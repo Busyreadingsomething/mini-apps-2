9289afed331e082b73712db52e7047586b5be5e6
Makan Azarshahy <oro1919@gmail.com>
2018-04-07 17:35:03 -0700

    Added conditional rendering for SquareView

--- challenge_3/my-app/src/components/SquareView.jsx
+++ challenge_3/my-app/src/components/SquareView.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const SquareView = ({ id }) => (
-  <div className="square" id={id}>
-    {id}
-  </div>
-);
+const SquareView = ({ id, row, col, selectSquare }) => {
+  const style = {};
+
+  if (id.revealed) {
+    style.backgroundColor = 'white';
+  } else {
+    style.backgroundColor = 'grey';
+  }
+
+  return (
+    <div
+      className="square"
+      id={`square${id.value}`}
+      row={row}
+      col={col}
+      onClick={e => selectSquare(e)}
+      style={style}>
+      {
+        id.revealed ? id.value : null
+      }
+    </div>
+  )
+};
 
 export default SquareView;
